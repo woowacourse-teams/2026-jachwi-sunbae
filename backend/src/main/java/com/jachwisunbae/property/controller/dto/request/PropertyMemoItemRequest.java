bc9e05d823dfc2c8154a0b3386b14492f411dc01
전원준 <jwjjwj1960@naver.com>
2026-09-03 09:45:49 +0900

    refactor: 구조화메모 삭제 및 메모 API 수정

--- backend/src/main/java/com/jachwisunbae/property/controller/dto/request/PropertyMemoItemRequest.java
+++
@@ -1,9 +0,0 @@
-package com.jachwisunbae.property.controller.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
-public record PropertyMemoItemRequest(
-        @NotNull Long systemMemoItemId,
-        @NotNull @Size(max = 100) String content) {
-}
--- backend/src/main/java/com/jachwisunbae/property/repository/query/PropertyMemoItemQuery.java
+++
@@ -1,5 +0,0 @@
-package com.jachwisunbae.property.repository.query;
-
-public record PropertyMemoItemQuery(Long propertyMemoItemId, Long systemMemoItemId,
-                                    String label, Integer displayOrder, String content) {
-}
